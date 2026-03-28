3c015bbb67509c076af99eb708556ef67725c04c
brunomorita <brunomorita6758@gmail.com>
2026-03-28 18:51:18 +0900

    Finished the snake game, it's kinda broken but it is something

--- Pyxel/snake.py
+++ Pyxel/snake.py
@@ -6,14 +6,26 @@ class Game:
         pyxel.init(160, 120, title="Snake")
         self.x = 75
         self.y = 55
+        # Fruit
+        self.apple_x = 5
+        self.apple_y = 5
+        self.generate_apple()
         self.vx = 2
         self.vy = 0
         self.speed = 2
         self.size = 5
         pyxel.run(self.update, self.draw)
 
+    def generate_apple(self):
+        # Ensure apple is within boundaries and matches grid
+        self.apple_x = pyxel.rndi(0, (160 // 5) - 1) * 5
+        self.apple_y = pyxel.rndi(0, (120 // 5) - 1) * 5
+
+    def snake_increase(self):
+        self.size += 1
+
     def update(self):
-        # Game over if hit the corner( close game)
+        # Game over if hit the corner (close game)
         if self.x >= pyxel.width or self.x <= 0:
             pyxel.quit()
 
@@ -36,14 +48,18 @@ class Game:
         self.x += self.vx
         self.y += self.vy
 
+        # collision with snake and apple must spawn randomly
+        if self.x <= self.apple_x and self.y <= self.apple_y:
+            self.size += 1
+            self.generate_apple()
+
     def draw(self):
         pyxel.cls(0)
 
         # Create an object(snake head)
-        pyxel.rect(self.x, self.y, self.size, self.size, 11)
+        pyxel.rect(self.x, self.y, 5, self.size, 11)
+
+        # Create apple
+        pyxel.rect(self.apple_x, self.apple_y, 5, 5, 8)
 
 Game()
-
-# Create fruit
-# Fruit must spawn randomly
-# If snake touches fruit it increases in length
